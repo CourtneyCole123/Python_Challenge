17a7afe8e9eebbbee6a57bacdc8ef7bb8860429e
CourtneyCole123 <162069113+CourtneyCole123@users.noreply.github.com>
2024-04-03 16:21:18 -0500

    fixed

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,30 @@
-import os
 import csv
 
+# set filepath
+budget_file_path = "PyBank/Resources/budget_data.csv"
 
-census_csv = os.path.join("..", "Resources", "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Financial Analysis
+#----------------------------
+#Total Months: 86
+#Total: $22564198
+#Average Change: $-8311.11
+#Greatest Increase in Profits: Aug-16 ($1862002)
+#Greatest Decrease in Profits: Feb-14 ($-1825558)
