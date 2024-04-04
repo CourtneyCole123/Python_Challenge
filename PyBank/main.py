1eb1354474db9ec87314d5b6957d675c24778af0
CourtneyCole123 <162069113+CourtneyCole123@users.noreply.github.com>
2024-04-04 12:47:48 -0500

    getting closer

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,8 +6,13 @@ budget_file_path = "PyBank/Resources/budget_data.csv"
 # set variables
 total_months = 0
 sum_profit_loss = 0
-monthly_change = 0
 profit_loss_list = []
+changes = []
+previous_month = 0
+monthly_change = 0
+profit_loss_dict = {}
+offset_list = []
+change = 0
 
 # open and read csv
 with open(budget_file_path) as budget_file:
@@ -19,32 +24,67 @@ with open(budget_file_path) as budget_file:
         month_date = row [0]
         profit_loss = row [1]
         sum_profit_loss= int(profit_loss)+ sum_profit_loss
-        profit_loss_list.append(row[1])
-    print(profit_loss_list)
+        profit_loss_list.append(row [1])
+   # print(profit_loss_list)
+        
+    current_month= int(row[1])
+    previous_month = 1088983
+
+    for row in profit_loss_list:
+        row = 0
+        monthly_change = int(profit_loss_list[row +1]) - int(profit_loss_list[row])
+        row = int(row+1)
+        
+        changes.append(monthly_change)
+    print(changes)
+
     
-    for profit_loss in profit_loss_list:
-        profit_loss = row [1]
-        monthly_change = int(profit_loss_list[0]) - int(profit_loss_list[0+1])
-    print(monthly_change)
 
 
+       # print(profit_loss)
 
+        #monthly_change = int(profit_loss_list [int(row)]) - int(profit_loss_list [int(row)-1])
+   
+   #     changes = []
+   #     changes.append(monthly_change)
+   #     total_change = int(changes[0]) + 1
+   # print(changes)
 
-
-
-            
+   # for row in profit_loss_list:
+    #    monthly_change = int(current_month) - int(previous_month)
+    #    current_month = int(current_month) +1
+#current_month = previous_month
+  #  print(monthly_change)
+       
+       
+       # for row in profit_loss_list:    
+       #     profit_loss = row [0]
+       #     monthly_change = int(profit_loss_list [1]) - int(profit_loss_list[0])
+       #     print(monthly_change)
         
-
-
-
-
-
+       
+       
+       
+       
+       
+       
+       # if total_months > 1:
+      #      change = current_month_profit_loss - previous_month_profit_loss
+       #     total_changes = change +1
+       #     previous_month_profit_loss = current_month_profit_loss
+   
+   
+   
+   
+   # for row in profit_loss_list:
+   #     monthly_change = int(profit_loss_list [int(0)]) - int(profit_loss_list [int(0)+1])
+   #     changes = []
+   #     changes.append(monthly_change)
+   #     total_change = int(changes[0]) + 1
+   # print(changes)
+    
+    
         
-
-
-
-
-
 
 
 #print(f'Financial Analysis')
